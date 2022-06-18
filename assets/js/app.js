3f748a3fdf3925ba0168a8dd0476633cced4f457
obemyshinnyrod <miswah.md@gmail.com>
2022-06-18 18:57:21 +0530

    (feat): faq section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,3 +61,12 @@ let barInterval = setInterval(() => {
 }, 20)
 
 
+const questions = [...document.querySelectorAll(".question")];
+
+questions.map((question) => {
+    let q_text = question.querySelector("h3");
+    q_text.addEventListener('click', () => {
+        questions.filter((q) => q !== question).map((q) => q.classList.remove('open'))
+        question.classList.toggle("open")
+    })
+})
--- main.js
+++ main.js
@@ -4,3 +4,5 @@ import "./assets/styles/base.scss";
 import "./assets/styles/loader.scss";
 import "./assets/styles/landing.scss";
 import "./assets/styles/skills.scss";
+import "./assets/styles/projects.scss";
+import "./assets/styles/faq.scss";
